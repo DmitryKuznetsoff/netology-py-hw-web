3db2c3b7b64fb3b1b7e04cc49eeb342c2ed47e4b
DmitryKuznetsoff <kuznetsoffd90@gmail.com>
2021-05-14 00:50:52 +0300

    hostname from .env file

--- flask/app/config.py
+++ flask/app/config.py
@@ -10,8 +10,7 @@ API_V1_URL_PREFIX = '/api/v1/'
 
 class Config(object):
     BASE_PATH = os.path.dirname(__file__).rstrip('/app')
-    SERVER_NAME = 'localhost'
-    SQLALCHEMY_DATABASE_URI = f'postgresql://{getenv("DB_USER")}:{getenv("DB_PWD")}@localhost:5432/{getenv("DB_NAME")}'
+    SQLALCHEMY_DATABASE_URI = f'postgresql://{getenv("DB_USER")}:{getenv("DB_PWD")}@{getenv("DB_HOSTNAME")}:5432/{getenv("DB_NAME")}'
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_SORT_KEYS = False
